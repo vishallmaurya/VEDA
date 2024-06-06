cb3acaffb06f7d39a91a60e45f7f689a45156adb
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-06-06 12:48:57 +0530

    code for getting data type of each column

--- iris.py
+++ iris.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('dirty_iris.csv')
+df = pd.read_csv('data\dirty_iris.csv')
 # print(df.head(5))
 # print(df.describe())
+df1 = pd.read_csv('temp_file.csv')
 
-print(df[df.duplicated()])
-df.drop_duplicates(inplace=True)
+print(len(df.columns[df.dtypes == 'object']))
+print(df1.dtypes)
 
-df
+
+# print(df[df.duplicated()])
+# df.drop_duplicates(inplace=True)
