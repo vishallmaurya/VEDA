d797a22bff3ae7d7acfefe8f2c70971d6dc4a123
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-06-06 17:10:18 +0530

    facing issue in identify numerical and categorical data

--- missing_value.py
+++ missing_value.py
@@ -30,8 +30,7 @@ def drop_row_column(df, datalosspercent = 10, limit = 0.04):
             new_df[excessive_null_columns] = df[excessive_null_columns]
 
             change = ((df.shape[0]-new_df.shape[0])/df.shape[0])*100
-            
-            print(new_df.shape)
+
             if change > datalosspercent:
                 return df
             else:
@@ -49,15 +48,35 @@ def impute_row_column(df, limit = 0.04):
             return df
         else:
             # checking for categorical column
-            categorical_column = list(limited_null_columns[df.dtypes == 'object'])
+            
+            categorical_column = [col for col in df.columns if col in limited_null_columns and df[col].dtype == 'object']
             numerical_column = [col for col in limited_null_columns if col not in categorical_column]
-
+            
             imputer1 = SimpleImputer(strategy='median')
-imputer2 = SimpleImputer(strategy='mean')
-def callingfunc():
-    df = pd.read_csv('data\data_science_job.csv')
-    print(df.shape)
-    df = drop_row_column(df)
-    print(df.shape)
+            imputer2 = SimpleImputer(strategy='mean')
+            imputer3 = SimpleImputer(strategy='constant',fill_value='Missing')
+            imputer4 = SimpleImputer(strategy='most_frequent')
 
+            trf = ColumnTransformer([
+                ('imputer1',imputer1,numerical_column),
+                # ('imputer2',imputer2,numerical_column)
+            ],remainder='passthrough')
+
+            new_data = df
+
+            new_data[numerical_column] = trf.fit_transform(df[numerical_column])
+            # print(new_data)
+            # new_data = pd.DataFrame(new_data[:, 0], columns=numerical_column)
+            # return new_data
+            return new_data
+
+def callingfunc():
+    # df = pd.read_csv('data\data_science_job.csv')
+    df = pd.read_csv('data\\titanic_toy.csv')
+    # print(df.shape)
+    # df = drop_row_column(df)
+    # df.info()
+    print(df.loc[df['Age'] == 22])
+    df = impute_row_column(df.drop(['Survived', 'Family'], axis = 1), limit=0.06)
+    print(df.loc[df['Age'] == 22])
 callingfunc()
