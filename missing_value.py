9fd7500402e893bb3234218e7c9b640f94318021
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-06-13 22:54:44 +0530

    error handling added

--- missing_value.py
+++ missing_value.py
@@ -48,21 +48,42 @@ def drop_row_column(df, datalosspercent = 10, limit = 0.04):
     univariate imputation on numerical column and categorical column
 """
 
-def impute_row_column(df, limit = 0.04, var_diff = 0.05, mod_diff = 0.05):
+def impute_row_column(df, limit = 0.04, var_diff = 0.05, mod_diff = 0.05, numerical_column = [], categorical_column = [], temporal_column = [], temporal_type = 'interpolate'):
+    
+    """
+        error handling
+    """
+    
+    if(limit > 1 or limit < 0):
+        raise ValueError("Invalid value. Value of limit should be from 0 to 1 inclusive.")
+    if(var_diff > 1 or var_diff < 0):
+        raise ValueError("Invalid value. Value of var_diff should be from 0 to 1 inclusive.")
+    if(mod_diff > 1 or mod_diff < 0):
+        raise ValueError("Invalid value. Value of mod_diff should be from 0 to 1 inclusive.")
+    dtype_list = ['object', 'category', 'string', 'interval', 'bool', ]
+
+    
     null_count = df.isnull().sum().values.sum()
     if null_count == 0: # if data is completely clean
         return df
     else:
         # columns that have less than given null values limit
         limited_null_columns = [var for var in df.columns if df[var].isnull().mean() <= limit]
-        excessive_null_columns = [var for var in df.columns if df[var].isnull().mean() > limit]
         
         if len(limited_null_columns) == 0:
             return df
         else:
             # checking for categorical column
-            categorical_column = [col for col in df.columns if col in limited_null_columns and (df[col].dtype == 'object' or df[col].dtype == 'category')]
-            numerical_column = [col for col in df.columns if col in limited_null_columns and (df[col].dtype == 'int64' or df[col].dtype == 'float64' or df[col].dtype == 'float32')]
+            
+            if len(categorical_column) == 0:
+                category_type = ['object', 'category', 'string', 'interval', 'bool']
+                categorical_column = [col for col in df.columns if col in limited_null_columns and (df[col].dtype in category_type)]
+            if len(categorical_column) == 0:
+                numeric_type = ['int64', 'int32', 'int16', 'int8', 'uint64', 'uint32', 'uint16', 'uint8', 'float64', 'float32']
+                numerical_column = [col for col in df.columns if col in limited_null_columns and (df[col].dtype in numeric_type)]
+            if len(temporal_column) == 0:
+                time_type = ['datetime64[ns]', 'timedelta64[ns]', 'datetime64[ns, tz]', 'period']
+                temporal_column = [col for col in df.column if col in limited_null_columns and (df[col].dtype in time_type)]
             
             imputer1 = SimpleImputer(strategy='median')
             imputer2 = SimpleImputer(strategy='mean')
@@ -87,6 +108,8 @@ def impute_row_column(df, limit = 0.04, var_diff = 0.05, mod_diff = 0.05):
             mean = trf2.fit_transform(new_data[numerical_column])
             mode = trf4.fit_transform(new_data[categorical_column])
 
+            # handling of numerical column
+
             for i in range(len(numerical_column)):
                 percentage_change_median = ((variation_before_imputation.iloc[i]-median[:, i].var())/variation_before_imputation.iloc[i])
                 percentage_change_mean = ((variation_before_imputation.iloc[i]-mean[:, i].var())/variation_before_imputation.iloc[i])
@@ -98,6 +121,8 @@ def impute_row_column(df, limit = 0.04, var_diff = 0.05, mod_diff = 0.05):
                 else:
                     new_data[numerical_column[i]] = df[numerical_column[i]]
 
+            # handling of categorical column
+
             for i in range(len(categorical_column)):
                 values = df[categorical_column[i]].value_counts().sort_values(ascending=False)
                 if values.iloc[1]/values.iloc[0] > mod_diff:
@@ -105,8 +130,13 @@ def impute_row_column(df, limit = 0.04, var_diff = 0.05, mod_diff = 0.05):
                 else:
                     new_data[categorical_column[i]] = mode[:, i]
 
-            return new_data
+            # handling of temporal column
 
+            for i in range(len(temporal_column)):
+                method_ = temporal_type + "()"
+                new_data[temporal_column[i]] = df[temporal_column[i]].method_
+
+            return new_data
 
 # multivariate knn Imputer
 
