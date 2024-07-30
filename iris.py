d93c0b6734209acadcf472537e249b9f1be2e5c9
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-07-30 11:15:35 +0530

    external callinb permitted

--- iris.py
+++ iris.py
@@ -1,18 +1,18 @@
+import missing_value as mv
+import dimensionality_reduction as dr
+import feature_selection as fs
+import outlier as out
 import pandas as pd
 
-data = pd.DataFrame({"text": ["Hello world!", "123 Main St", "This is some text."]})
+df = pd.read_csv('data\data_science_job.csv')
+X = df.drop('target', axis=1)
+y = df['target']
 
-# Check for object dtype
-if pd.api.types.is_object_dtype(data["text"]):
-  print("The 'text' column might contain textual data.")
+print("Initial shape:  ", X.shape, " and ", y.shape)
 
-# Check for alphanumeric characters
-is_alnum = data["text"].str.isalnum()
-print(f"Checking : {is_alnum}")
-print("'text' column has alphanumeric characters:", is_alnum.all())
+X, y = mv.callingfunc(X, y)
+outlier, X, y = out.callingfun(X, y)
+X, y = fs.callingfunc(X, y)
+X, y = dr.callingfunc(X, y)
 
-# Use regular expression (example: check for digits at the beginning)
-import re
-pattern = r"^\d+"
-has_digits_at_start = data["text"].str.contains(pattern)
-print("'text' column has digits at the start:", has_digits_at_start.any())
+print("Final shape:  ", X.shape, " and ", y.shape)
