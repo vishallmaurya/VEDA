5e981c7e2035b91f41d22e272a1ac0ee013fa027
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-08-01 07:59:53 +0530

    testing code added

--- iris.py
+++ iris.py
@@ -4,9 +4,9 @@ import feature_selection as fs
 import outlier as out
 import pandas as pd
 
-df = pd.read_csv('data\data_science_job.csv')
-X = df.drop('target', axis=1)
-y = df['target']
+df = pd.read_csv('data\dirty_iris.csv')
+X = df.drop('Species', axis=1)
+y = df['Species']
 
 print("Initial shape:  ", X.shape, " and ", y.shape)
 
