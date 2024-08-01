0f79595acdda44b5a8a931eb0b6740a6bbc907b7
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-08-01 20:43:44 +0530

    added

--- iris.py
+++ iris.py
@@ -4,9 +4,9 @@ import feature_selection as fs
 import outlier as out
 import pandas as pd
 
-df = pd.read_csv('data\data_science_job.csv')
-X = df.drop('target', axis=1)
-y = df['target']
+df = pd.read_csv('data\placement.csv')
+X = df.drop('placed', axis=1)
+y = df['placed']
 
 print("Initial shape:  ", X.shape, " and ", y.shape, " and ", type(y))
 
