7f4bb717c5d6afb1f55624c5c224c5d23776b327
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-08-02 19:53:24 +0530

    check code added

--- iris.py
+++ iris.py
@@ -3,10 +3,13 @@ import dimensionality_reduction as dr
 import feature_selection as fs
 import outlier as out
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-df = pd.read_csv('data\placement.csv')
-X = df.drop('placed', axis=1)
-y = df['placed']
+df = pd.read_csv('data\Student_performance_data _.csv')
+X = df.drop('GradeClass', axis=1)
+y=df['GradeClass']
 
 print("Initial shape:  ", X.shape, " and ", y.shape, " and ", type(y))
 
@@ -16,3 +19,12 @@ X, y = fs.callingfunc(X, y)
 X, y = dr.callingfunc(X, y)
 
 print("Final shape:  ", X.shape, " and ", y.shape)
+
+x_train,x_test,y_train,y_test=train_test_split(X,y,train_size=0.8,random_state=42,stratify=y)
+
+model=RandomForestClassifier()
+model.fit(x_train,y_train)
+
+y_pred=model.predict(x_test)
+
+print(accuracy_score(y_pred,y_test))
