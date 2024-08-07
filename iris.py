ed11deb7bf509836743d8e45f6f2364c12904643
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-08-07 13:12:35 +0530

    added

--- iris.py
+++ iris.py
@@ -16,9 +16,9 @@ from sklearn.ensemble import RandomForestRegressor, VotingRegressor, RandomFores
 # from xgboost import XGBRegressor, XGBClassifier
 # from catboost import CatBoostRegressor, CatBoostClassifier
 
-df = pd.read_csv('data\\alzheimers_disease_data.csv')
-y = df['Diagnosis']
-X = df.drop('Diagnosis', axis=1)
+df = pd.read_csv('data\campaign_data.csv')
+y = df['IsSuccessful']
+X = df.drop('IsSuccessful', axis=1)
 
 print("Initial shape:  ", X.shape, " and ", y.shape, " and ", type(y))
 
@@ -50,23 +50,13 @@ if(strategy == 'ensemble'):
     # Confusion Matrix
     print("Confusion Matrix:")
     print(confusion_matrix(Y_test, y_pred))
+else:
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, Y_train)
 
-rf = RandomForestClassifier(random_state=42)
-param_grid = {
-    'n_estimators': [100, 200, 300],
-    'max_depth': [None, 10, 20, 30],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4]
-}
+    y_pred = model.predict(X_test)
 
-grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
-grid_search.fit(X_train, Y_train)
+    accuracy = accuracy_score(Y_test, y_pred)
+    print(f'Accuracy: {accuracy:.2f}')
 
-print(f"Best parameters: {grid_search.best_params_}")
-
-best_rf = grid_search.best_estimator_
-y_pred = best_rf.predict(X_test)
-
-print(classification_report(Y_test, y_pred))
-
-print(confusion_matrix(Y_test,y_pred))
+    print(classification_report(Y_test, y_pred))
