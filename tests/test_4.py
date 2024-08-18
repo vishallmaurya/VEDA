101dc65dc7dad4555a1b2f0e0d935c2b389398a5
vishallmaurya <115442999+vishallmaurya@users.noreply.github.com>
2024-08-18 15:35:10 +0530

    data input type changed

--- tests/test_4.py
+++ tests/test_4.py
@@ -15,7 +15,7 @@ def test():
     csv_path = os.path.join('tests', 'sample_data', 'sample.csv')
     
     # Load data
-    df = pd.read_csv(csv_path)
+    df = pd.read_csv(csv_path,encoding='ISO-8859-1')
 
     X = df.drop(['price'], axis=1)
     y = df['price']
