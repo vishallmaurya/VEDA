3e6e298ec7f70b4b4632a2b3d596c71955f50d31
vishallmaurya <vishallmaurya210@gmail.com>
2025-09-27 12:07:32 +0530

    IterativeImputer in scikit-learn is still experimental, so before importing it must explicitly enable it using

--- src/veda_lib/Preprocessor.py
+++ src/veda_lib/Preprocessor.py
@@ -23,6 +23,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer  
 from sklearn.impute import SimpleImputer, KNNImputer, IterativeImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
